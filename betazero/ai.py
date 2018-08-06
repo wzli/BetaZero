0cb2ec46ec02a69f1e5be24ef44442d69e0fcf48
WenZheng Li <me@wenzheng.li>
2018-08-07 02:12:00 +0900

    factored out min_max_propagate function

--- betazero/ai.py
+++ betazero/ai.py
@@ -24,27 +24,30 @@ class Agent:
         value_samples = np.array((self.game.sample_pdf(pdf) for pdf in value_pdfs))
         return actions[np.argmax(value_samples)]
 
+    def min_max_propagate(self, value, reset_count):
+        training_input_set = [self.game.input_transform(input_state)
+                for input_state in self.state_history[-reset_count:]]
+        training_target_set = [one_hot_pdf(value, self.game.max_value, self.game.output_dimension)]
+        for chosen_state, (_, state_transitions, _, _, value_pdfs) in zip(
+                reversed(self.state_history[-reset_count:-1]),
+                reversed(self.action_prediction_history[-reset_count:-1]
+                )):
+            _, chosen_state_bytes = self.game.reduce_symetry(-chosen_state)
+            state_transition_bytes = [state_transition.tobytes()
+                    for state_transition in state_transitions]
+            action_index = state_transition_bytes.index(chosen_state_bytes)
+            value_pdfs[action_index] = np.flip(training_target_set[-1])
+            training_target_set.append(max_pdf(value_pdfs))
+        training_target_set = reversed(training_target_set)
+        return (training_input_set, training_target_set)
+
     def update_session(self, state, value, reset_count):
         self.state_history.append(state)
         if reset_count is 0:
             self.action_prediction_history.append(self.generate_predictions(state))
-        else:
-            if value is not None:
-                training_input_set = [self.game.input_transform(input_state)
-                        for input_state in self.state_history[-reset_count:]]
-                training_target_set = [one_hot_pdf(value, self.game.max_value, self.game.output_dimension)]
-                for chosen_state, (_, state_transitions, _, _, value_pdfs) in zip(
-                        reversed(self.state_history[-reset_count:-1]),
-                        reversed(self.action_prediction_history[-reset_count:-1]
-                        )):
-                    _, chosen_state_bytes = self.game.reduce_symetry(-chosen_state)
-                    state_transition_bytes = [state_transition.tobytes()
-                            for state_transition in state_transitions]
-                    action_index = state_transition_bytes.index(chosen_state_bytes)
-                    value_pdfs[action_index] = np.flip(training_target_set[-1])
-                    training_target_set.append(max_pdf(value_pdfs))
-                training_target_set = reversed(training_target_set)
-                self.pdfs = training_target_set
-                self.state_history = self.state_history[:-reset_count]
-                self.action_prediction_history = self.action_prediction_history[:-reset_count]
-                self.action_prediction_history.append(self.generate_predictions(self.state_history[-1]))
+        elif value is not None:
+            self.inputs, self.pdfs = self.min_max_propagate(value, reset_count)
+            #todo train model here
+            self.state_history = self.state_history[:-reset_count]
+            self.action_prediction_history = self.action_prediction_history[:-reset_count]
+            self.action_prediction_history.append(self.generate_predictions(self.state_history[-1]))
