31c99b791ae52682c3a781f5ddb7841a7870647c
WenZheng Li <me@wenzheng.li>
2018-08-25 19:51:21 +0900

    chinese chess wip

--- betazero/chinese_chess.py
+++ betazero/chinese_chess.py
@@ -1,11 +1,21 @@
 import numpy as np
 
+# keras model
 board_size = (10, 9)
+input_dimensions = (*board_size, 7)
+output_dimension = 51
+max_value = 25
+min_max = True
+rotational_symetry = False
+vertical_symetry = True
+horizontal_symetry = True
+# end states of the game may corresponds to terminal nodes in the state tree
+terminal_state = True
+reward_span = 6
 
 
 def is_within_bounds(move):
     return move[0] >= 0 and move[0] < board_size[0] and move[1] >= 0 and move[1] < board_size[1]
-
 
 class Piece:
     def __init__(self, board, pieces, player, spawn_location):
@@ -31,8 +41,10 @@ class Piece:
             return False
         return True
 
-    def move(self, move):
+    def move(self, move, check_invalid = True):
         reward = 0
+        if check_invalid and move not in self.get_moves():
+            return self.board, -max_value
         if self.board[move]:
             reward = self.board[move].reward
             self.peices[-self.player].remove(self.board[move])
@@ -41,8 +53,10 @@ class Piece:
         self.location = move
         return self.board, reward
 
-    def test(self, move):
+    def test(self, move, check_invalid = False):
         board = np.copy(self.board)
+        if check_invalid and move not in self.get_moves():
+            return board, -max_value
         reward = board[move].reward if board[move] else 0
         board[move] = self
         board[self.location] = None
@@ -50,9 +64,7 @@ class Piece:
 
 
 class Pawn(Piece):
-    @property
-    def reward(self):
-        return 2 if self.is_across_river() else 1
+    reward = 1
 
     def get_moves(self):
         row, col = self.location
@@ -68,7 +80,7 @@ class Pawn(Piece):
 
 
 class Guard(Piece):
-    reward = 2
+    reward = 1
 
     def get_moves(self):
         row, col = self.location
@@ -83,7 +95,7 @@ class Guard(Piece):
 
 
 class King(Piece):
-    reward = 50
+    reward = 25
 
     def get_moves(self):
         row, col = self.location
@@ -102,11 +114,11 @@ class King(Piece):
         return moves
 
     def __str__(self):
-        return 'Q' if self.player > 0 else 'q'
+        return 'K' if self.player > 0 else 'k'
 
 
-class Bishop(Piece):
-    reward = 2
+class Elephant(Piece):
+    reward = 1
 
     def get_moves(self):
         row, col = self.location
@@ -117,11 +129,11 @@ class Bishop(Piece):
         ]
 
     def __str__(self):
-        return 'B' if self.player > 0 else 'b'
+        return 'E' if self.player > 0 else 'e'
 
 
 class Knight(Piece):
-    reward = 4
+    reward = 2
 
     def get_moves(self):
         row, col = self.location
@@ -135,11 +147,11 @@ class Knight(Piece):
         return moves
 
     def __str__(self):
-        return 'K' if self.player > 0 else 'k'
+        return 'N' if self.player > 0 else 'n'
 
 
 class Rook(Piece):
-    reward = 9
+    reward = 4
 
     def get_moves(self):
         row, col = self.location
@@ -158,7 +170,7 @@ class Rook(Piece):
 
 
 class Cannon(Piece):
-    reward = 4.5
+    reward = 2
 
     def get_moves(self):
         row, col = self.location
@@ -193,8 +205,8 @@ default_spawn = (None, (
     (Cannon, (2, 7)),
     (Knight, (0, 1)),
     (Knight, (0, 7)),
-    (Bishop, (0, 2)),
-    (Bishop, (0, 6)),
+    (Elephant, (0, 2)),
+    (Elephant, (0, 6)),
     (Guard, (0, 3)),
     (Guard, (0, 5)),
     (King, (0, 4)),
@@ -210,8 +222,8 @@ default_spawn = (None, (
     (Cannon, (7, 7)),
     (Knight, (9, 1)),
     (Knight, (9, 7)),
-    (Bishop, (9, 2)),
-    (Bishop, (9, 6)),
+    (Elephant, (9, 2)),
+    (Elephant, (9, 6)),
     (Guard, (9, 3)),
     (Guard, (9, 5)),
     (King, (9, 4)),
@@ -230,10 +242,23 @@ class Session:
         for player in (1, -1):
             for piece, location in default_spawn[player]:
                 piece(self.board, self.pieces, player, location)
+        return State(self, np.copy(self.board), self.player), 0, 0
+
+    def do_action(self, check_invalid = True):
+        if not self.board[location] or self.board[location].player != self.player:
+            return State(self, np.copy(self.board), self.player), -max_value, 1
+        board, reward = self.board[location].move(move)
+        if reward < 0:
+            reset = 1
+        else:
+            reset = 0
+            self.n_turns += 1
+        if reward >= max_value:
+            reset = self.n_turns
+        return State(self, np.copy(self.board), self.player), reward, reset
 
 
 #------------ The below is required game interface for betazero
-
 
 class State:
     def __init__(self, session, board, player):
@@ -249,7 +274,7 @@ class State:
             tuple(
                 np.vectorize(
                     lambda x: x.player * self.player if isinstance(x, piece) else 0
-                )(self.board) for piece in (Pawn, Rook, Cannon, Knight, Bishop, Guard, King)))[np.newaxis]
+                )(self.board) for piece in (Pawn, Rook, Cannon, Knight, Elephant, Guard, King)))[np.newaxis]
 
     def key(self):
         return self.board.tobytes()
@@ -261,20 +286,20 @@ class State:
 
 def get_actions(state):
     """Returns the list of all valid actions given a game state."""
-    return [(piece, move) for piece in state.session.pieces[state.player]
+    return [(piece.location, move) for piece in state.session.pieces[state.player]
             for move in piece.get_moves()]
 
 
 def predict_action(state, action):
-    piece, move = action
-    return State(state.session, piece.test(move), state.player)
+    location, move = action
+    board, reward = state.board[location].test(move)
+    reset = state.session.n_turns + 1 if reward >= max_value else 0
+    return State(state.session, board, state.player), reward, reset
 
-
-session = Session()
-
-state = State(session, session.board, 1)
-
-for piece, move in get_actions(state):
-    print(piece, piece.location, move)
-
-print(state)
+#session = Session()
+#state = State(session, session.board, 1)
+#
+#for action in get_actions(state):
+#    state_transition, reward, reset = predict_action(state, action)
+#    print(state_transition, action)
+#print(state)
