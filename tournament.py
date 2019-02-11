4e6ff93b5fa17b2b237acf531ee984a6bf478978
WenZheng Li <me@wenzheng.li>
2019-02-12 04:13:22 +0900

    changed to heap based tournament

--- tournament.py
+++ tournament.py
@@ -1,48 +1,7 @@
 #!/usr/bin/env python3
 import argparse, os, yaml
-from collections import deque
+from heapq import heapify, heapreplace
 from betazero import ai, utils
-
-agents = {}
-
-
-def play_match(game, player1, player2, model_path, matches=2):
-    for player in (player1, player2):
-        if player not in agents:
-            agents[player] = ai.Agent(
-                game, str(player),
-                os.path.join(model_path, "model_" + str(player) + ".h5"))
-    arena = utils.Arena(
-        game,
-        agents[player1],
-        agents[player2],
-        print_actions=False,
-        matches=matches)
-    print("")
-    if arena.stats[1] > arena.stats[-1]:
-        winner = player1
-        loser = player2
-    elif arena.stats[1] < arena.stats[-1]:
-        winner = player2
-        loser = player1
-    elif arena.score > 0:
-        winner = player1
-        loser = player2
-    else:
-        winner = player2
-        loser = player1
-    return winner, loser
-
-
-def adjust_elo(elo_record, winner, loser, k):
-    rating_gap = (
-        elo_record.setdefault(winner, 0) - elo_record.setdefault(loser, 0)
-    ) / 400
-    p_winner = 1.0 / (1.0 + 10**-rating_gap)
-    p_loser = 1.0 - p_winner
-    elo_record[winner] += k * (1 - p_winner)
-    elo_record[loser] -= k * p_loser
-
 
 def scan_models(directory):
     models = set()
@@ -54,6 +13,58 @@ def scan_models(directory):
                 models.add(int(tokens[1]))
     return models
 
+class Tournament:
+    def __init__(self, game):
+        self.game = game
+        self.participants = []
+
+    def playoff(self, participant1, participant2):
+        arena = utils.Arena(
+            self.game,
+            participant1.agent,
+            participant2.agent,
+            print_actions=False,
+            matches=self.matches)
+        print("")
+        if arena.stats[1] > arena.stats[-1]:
+            winner = participant1
+            loser = participant2
+        elif arena.stats[1] < arena.stats[-1]:
+            winner = participant2
+            loser = participant1
+        elif arena.score > 0:
+            winner = participant1
+            loser = participant2
+        else:
+            winner = participant2
+            loser = participant1
+        return winner, loser
+
+    def run_once(self, matches):
+        self.matches = matches
+        # de-throne the champion
+        heapreplace(self.participants, self.participants[0])
+        # itterate tornament
+        heapify(self.participants)
+
+    def eliminate(self, n_remaining):
+        if n_remaining >= len(self.participants):
+            return []
+        eliminated = self.participants[n_remaining:]
+        self.participants = self.participants[:n_remaining]
+        return eliminated
+
+class TournamentParticipant:
+    def __init__(self, tournament, model_directory, agent_id):
+        self.tournament = tournament
+        self.id = agent_id
+        self.agent = ai.Agent(tournament.game, str(agent_id),
+                os.path.join(model_directory, "model_" + str(agent_id) + ".h5"))
+
+    def __lt__(self, opponent):
+        winner, loser = tournament.playoff(self, opponent)
+        # it's a min heap, less is good
+        return winner.id == self.id
 
 if __name__ == '__main__':
     # create command line arguments
@@ -61,25 +72,27 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='BetaZero Tournament App')
     parser.add_argument("game", choices=games, help='select game')
     parser.add_argument(
+        '-n',
+        "--n-participants",
+        default=15,
+        type=int,
+        help='number of matchs per playoff')
+    parser.add_argument(
         '-m',
         "--matches",
+        type=int,
         default=2,
-        help='number of matchs per elo adjustment')
+        help='number of matchs per playoff')
     parser.add_argument(
-        '-r',
-        "--elo-record",
-        default='elo_record.yaml',
-        help='path to the elo record')
+        '-f',
+        "--record-file",
+        default='record.yaml',
+        help='path to the record file')
     parser.add_argument(
         '-d',
         "--model-directory",
         default='.',
         help='directory containing models_[ts].h5 files')
-    parser.add_argument(
-        '-k',
-        "--elo-k",
-        default=30,
-        help='tune sensitivity of elo adjustments')
     args = parser.parse_args()
     # load the module corresponding to selected game
     print("seleted game:", args.game)
@@ -89,39 +102,36 @@ if __name__ == '__main__':
         from betazero import chinese_chess as game
     elif args.game == games[2]:
         from betazero import tic_tac_toe as game
-    # read/create elo_record yaml file
-    file_mode = 'r+' if os.path.exists(args.elo_record) else 'w+'
-    with open(args.elo_record, file_mode) as elo_record_yaml:
-        # load stats from yaml file
-        elo_record = yaml.load(elo_record_yaml) or {}
+    # create the tournament
+    tournament = Tournament(game)
+    # read/create record yaml file
+    file_mode = 'r+' if os.path.exists(args.record_file) else 'w+'
+    with open(args.record_file, file_mode) as record_yaml:
+        # load record from yaml file
+        record = yaml.load(record_yaml) or {"participants":[], "eliminated":set()}
         # find models matching names "model_[ts].h5"
         models = scan_models(args.model_directory)
-        # see which models have an existing record
-        old_models = models & elo_record.keys()
-        new_models = models - old_models
-        # add old models to queue, sorted by decending elo
-        challenger_queue = deque(
-            sorted(old_models, key=elo_record.get, reverse=True))
-        # pickout highest elo champion
-        if old_models:
-            champion = challenger_queue.popleft()
-        # add new models to front of queue, sorted by decending timestamp
-        challenger_queue.extendleft(sorted(new_models, reverse=True))
-        # pickout most recent if no models have a record
-        if not old_models:
-            champion = challenger_queue.popleft()
-        # keep playing matches, winner stays on
+        # initialize participants
+        for model in models - record["eliminated"]:
+            tournament.participants.append(TournamentParticipant(tournament, args.model_directory, model))
         while True:
-            scanned_models = scan_models(args.model_directory)
-            challenger_queue.extendleft(
-                sorted(scanned_models - models, reverse=True))
+            # print current record
+            print("Rankings:")
+            for rank, participant in enumerate(record["participants"]):
+                print(rank, "\t", participant)
+            for participant in record["eliminated"]:
+                print("e\t", participant)
+            print("")
+            # scan for new participants to add to the tournament
+            scanned_models = scan_models(args.model_directory) - record["eliminated"]
+            for model in scanned_models - models:
+                tournament.participants.append(TournamentParticipant(tournament, args.model_directory, model))
             models = scanned_models
-            challenger = challenger_queue.popleft()
-            champion, loser = play_match(game, challenger, champion,
-                                         args.model_directory, args.matches)
-            challenger_queue.append(loser)
-            adjust_elo(elo_record, champion, loser, args.elo_k)
-            elo_record_yaml.seek(0)
-            elo_record_yaml.truncate()
-            yaml.dump(elo_record, elo_record_yaml, default_flow_style=False)
-        print(elo_record)
+            tournament.run_once(args.matches)
+            eliminated_participants = tournament.eliminate(args.n_participants)
+            record["participants"] = [participant.id for participant in tournament.participants]
+            record["eliminated"].update({participant.id for participant in eliminated_participants})
+            # record the record
+            record_yaml.seek(0)
+            record_yaml.truncate()
+            yaml.dump(record, record_yaml, default_flow_style=False)
