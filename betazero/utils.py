89e93fec3b53673ab904ba25b915fda89c4437da
WenZheng Li <me@wenzheng.li>
2019-02-18 17:39:29 +0000

    fixed timeout exception

--- betazero/utils.py
+++ betazero/utils.py
@@ -8,21 +8,17 @@ from contextlib import contextmanager
 @contextmanager
 def timeout(time):
     # Register a function to raise a TimeoutError on the signal.
+    def raise_timeout(signum, frame):
+        raise TimeoutError
     signal.signal(signal.SIGALRM, raise_timeout)
     # Schedule the signal to be sent after ``time``.
     signal.alarm(time)
     try:
         yield
-    except TimeoutError:
-        pass
     finally:
         # Unregister the signal so it won't be triggered
         # if the timeout is not reached.
         signal.signal(signal.SIGALRM, signal.SIG_IGN)
-
-
-def raise_timeout(signum, frame):
-    raise TimeoutError
 
 
 def clip(value, upper, lower=0):
